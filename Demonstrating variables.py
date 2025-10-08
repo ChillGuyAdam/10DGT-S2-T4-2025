c645f5029e9a9e9f0e5a612b846d9d9efc34e22d
ChillGuyAdam <andre.vorster@kingsway.school.nz>
2025-10-08 15:00:41 +1300

    version 2.0
    Demonstrating variables version 2.0

--- Demonstrating variables.py
+++ Demonstrating variables.py
@@ -1,11 +1,52 @@
 #Variables
 #Demonstrate how variables are created and how they work
 #Author: Andre Vorster
-#Date: 17/9/2025
-#Version 1.0 
+#Date: 08/10/2025
+#Version 2.0
 
 #Different types of variables
 #Store a string
 
 greeting = 'Hello World'
 print(greeting)
+
+#Store a number:
+my_number = 80 #assigned 80 to the vairiable called my_number
+print(my_number)
+
+my_number2 = 2
+print(my_number2)
+
+#Assign the value of one variable to another
+my_number = greeting
+print(my_number)
+
+
+''' This is a comment
+and 
+I
+Can press enter
+as
+many
+times
+as
+I
+want!''' 
+
+#Creating a new variable
+
+num_1 = 5
+num_2 = 18
+
+sum1 = num_1 + num_2
+print(sum1)
+
+#Concatination:adding two strings together
+sum_string1 = "18"
+sum_string2 = "5"
+
+sum = sum_string1 + sum_string2
+print(sum)
+
+#Print Formatting
+print(f"My calculations for adding {num_1} and {num_2} is equal to {sum1}.")
