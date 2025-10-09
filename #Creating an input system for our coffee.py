6c9ae7961106d295e017e55f1453af03a2e3dbad
ChillGuyAdam <andre.vorster@kingsway.school.nz>
2025-10-10 11:07:39 +1300

    Main program created

---
+++ #Creating an input system for our coffee.py
@@ -0,0 +1,33 @@
+#Creating an input system for our coffee prgram
+#Author: André Vorster
+#Date: 10/10/25
+#Version: 1.0
+
+#  TODO:Ask the user if they like coffee 
+#       Record the answer
+#       Give a response back to the answer
+
+#Ask the user if they want coffee
+'''LikeCoffee = input("Do you like coffee?       ")
+print(f'You answered "{LikeCoffee}".')
+
+if LikeCoffee == "yes" or "Yes" or "YES": 
+    print("That is great!I like coffee too!")
+
+else: 
+    print("That's ok!")'''
+
+#Version 2
+#While loop
+keepGoing = ""
+while keepGoing == "": 
+    likeCoffee = input("Do you like coffee? ")
+
+    if likeCoffee == "Yes": 
+        print("That is great! I like coffee too!")
+
+    elif likeCoffee == "No":
+        print("That's ok!")
+
+    else:
+        print("I don't understand. Please answer with Yes or No")
