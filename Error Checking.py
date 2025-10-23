6884c5f2ddc86e608e2296bdf7e3481a14967063
ChillGuyAdam <andre.vorster@kingsway.school.nz>
2025-10-24 09:59:42 +1300

    Version 1.4 of Error checking

--- Error Checking.py
+++ Error Checking.py
@@ -48,7 +48,7 @@ print(f"Then number that you entered is {num}.") '''
 test_int_num() #This calls the function
 '''
 
-#Code that tests wheter a valid input is given (v1.3)
+'''#Code that tests wheter a valid input is given (v1.3)
 #Use the funciton parameters to make my code more 
 #pythonic. 
 
@@ -74,5 +74,41 @@ print(f"Your first number you entered is {num1}.")
 num2 = test_int("Please enter your second number:")
 print (f"Your second number you entered is {num2}")
 
+sum = num1 + num2 
+print(f"Your numbers added together equal to {sum}.") '''
+
+#Code that tests wheter a valid input is given (v1.3)
+#Use the funciton parameters to make my code more 
+#pythonic. 
+#version 1.4
+
+def valid_number(question, low, high): 
+    error = "whoops! that is not an integer between {low} and {high}" 
+    while True: 
+        try: 
+            response = int(input(question))
+            if low <= response <= high: 
+                break # This stops the loop from line 86.
+
+            else: 
+                print (f"{error}\n")
+        except ValueError: 
+            print(error)
+    return response#This makes the response avalaible to be used 
+
+#Main routine: 
+if __name__ == "__main__": 
+
+    num_1 = valid_number("enter a number between 1 and 10: ",1,10)
+    print (f"You entered the number{num_1}\n")
+
+    num_2 = valid_number("Enter a number between 15 and 25: ", 15, 25)
+    print(f"You entered {num_2}\n") 
+
+    num_3 = valid_number("Enter a number between 70 nd 85",70,85) 
+    print(f"You entered {num_3}\n") 
+                    
+    sum = num_1 + num_2 + num_3
+    print(f"The total sum of {num_1} and {num_2} and {num_3} is {sum}. \n")
 
 
