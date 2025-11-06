55119eeec9dfd691bd5cc8c18e8ad658232ad9ae
ChillGuyAdam <andre.vorster@kingsway.school.nz>
2025-11-07 10:49:41 +1300

    v1.1 of fence cost calculator

--- #Creating a fence - cost calculator.py
+++ #Creating a fence - cost calculator.py
@@ -1,18 +1,18 @@
 #Creating a fence - cost calculator
 #Author: André Vorster
 #Date: 7 November 2025
-#Version: 1.0
+#Version: 1.1
 #Main routine starts here 
 keep_going = ""
-error = "Please enter an number (higher than 0)"
+error = "Please enter an number (higher than 0)\n"
 width = 0 
 while keep_going == "": 
     while width <= 0:
         #get width and height and display it
         try:
-            width = float(input("What is your width?:   "))
+            width = float(input("What is your width in metres?:   "))
             if width > 0: 
-                print(f"Your width is {width}") 
+                print(f"Your width is {width}m") 
             else:
                 print(error)
         except ValueError:
@@ -21,16 +21,27 @@ while keep_going == "":
     length = 0
     while length <= 0: 
         try: 
-            length = float(input("What is your length?:  "))
+            length = float(input("\nWhat is your length in metres?:  "))
             if length > 0: 
-                print(f"Your length is {length}")
+                print(f"Your length is {length}m")
             else:
                 print(error)
         except ValueError: 
             print(error) 
     perimeter = 2 * (width + length) 
-    cost_per_meter = float(input(f"How much does it cost per metre of fence?:   ")) 
+    cost_per_meter = 0
+    while cost_per_meter <= 0: 
+        try: 
+            cost_per_meter = float(input(f"\nHow much does it cost per metre of fence?:   $")) 
+            if cost_per_meter > 0: 
+                print(f"The cost per meter is ${cost_per_meter}")
+            else: 
+                print(error)
+        except ValueError: 
+            print(error) 
+        
     #Calculate the perimiter of the fence, and how much the total fence costs: 
     total_cost = cost_per_meter * perimeter
-    print(f"So with your total perimeter of {perimeter}m and the cost of ${cost_per_meter} per meter of fence,\n the total cost to fully fence the desired area is ${total_cost}.") 
-    keep_going = input("Please press <Enter> for another calculation or type\nany key and press enter to close program. ")
+    print(f"\nSo with your total perimeter of {perimeter}m and the cost of ${cost_per_meter} per meter of fence,\nthe total cost to fully fence the desired area is ${total_cost}.") 
+    keep_going = input("\n\nPlease press <Enter> for another calculation or type\nany key and press enter to close program. ")
+print("Thank you for using the calculator!") 
