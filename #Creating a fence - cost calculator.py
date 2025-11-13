a1d7588a1cbd8f084f4f9c2ab3591ed37aceb9ac
ChillGuyAdam <andre.vorster@kingsway.school.nz>
2025-11-14 09:59:21 +1300

    v1.2
    Something went wrong with all the uplaods of this file, so I restarted it to Version 1, I think I mentioned this in the previous upload. SO basically I didn't update visual studio code and it gave me problems, so now I just uploaded it. But I think this is my fifth time editing it. (Added comments to this one to make to code more understandable, for me at least.)

--- #Creating a fence - cost calculator.py
+++ #Creating a fence - cost calculator.py
@@ -1,11 +1,15 @@
 #Creating a fence - cost calculator
 #Author: André Vorster
-#Date: 7 November 2025
-#Version: 1.1
+#Date: 14 November 2025
+#Version: 1.2
 #Main routine starts here 
+
+#Creating variables: keep_going to help loop the program at the end and error, so that I can print an error and not use 
+#any extra characters than I need to to print error messages. 
 keep_going = ""
 error = "Please enter an number (also has to be higher than 0)\n" 
-width = 0 
+
+width = 0  #takin in the desired width of the user and displaying it. If it doesn't work then print error
 while keep_going == "": 
     while width <= 0:
         #get width and height and display it
@@ -18,7 +22,8 @@ while keep_going == "":
         except ValueError:
             print(error) 
         #looping the length
-    length = 0
+
+    length = 0 #taking in the desired length of the user and displaying it. If it doesn't work then print error
     while length <= 0: 
         try: 
             length = float(input("\nWhat is your length in metres?:  "))
@@ -28,8 +33,10 @@ while keep_going == "":
                 print(error)
         except ValueError: 
             print(error) 
-    perimeter = 2 * (width + length) 
-    cost_per_meter = 0
+
+    perimeter = 2 * (width + length) #calculate the perimeter to print later
+    cost_per_meter = 0 #taking in the cost per meter of fence and printing it. Otherwise if something goes wrong
+    # print an error message if it doesn't work. 
     while cost_per_meter <= 0: 
         try: 
             cost_per_meter = float(input(f"\nHow much does it cost per metre of fence?:   $")) 
@@ -40,8 +47,9 @@ while keep_going == "":
         except ValueError: 
             print(error) 
         
-    #Calculate the perimiter of the fence, and how much the total fence costs: 
+    #Calculate the perimeter of the fence, and how much the total fence costs, then printing it and showing the results to the user:  
     total_cost = cost_per_meter * perimeter
     print(f"\nSo with your total perimeter of {perimeter}m and the cost of ${cost_per_meter} per meter of fence,\nthe total cost to fully fence the desired area is ${total_cost}.") 
+    #Ask the user if they want another calculation or if they want to stop the program. 
     keep_going = input("\n\nPlease press <Enter> for another calculation or type\nany key and press enter to close program. ")
 print("Thank you for using the calculator!") 
